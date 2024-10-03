9ccbaafd7b59199be286a441c5f45215e955d8cc
gucio321 <gucio321@protonmail.com>
2024-10-03 10:45:37 +0200

    main: add clipboard support

--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/gucio321/mati
 
 go 1.23.1
 
-require github.com/PuerkitoBio/goquery v1.10.0
+require (
+	github.com/PuerkitoBio/goquery v1.10.0
+	golang.design/x/clipboard v0.7.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
 	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- skrypt.go
+++ skrypt.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -11,44 +12,67 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.design/x/clipboard"
 )
 
 const WorkingUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 
-var URL, DIR string
+var (
+	URL, DIR         string
+	supportClipboard bool
+	useClipboard     bool
+)
 
 func main() {
+	// 0: initialization
+	// 0.1: check if clipboard is supported
+	// Init returns an error if the package is not ready for use.
+	err := clipboard.Init()
+	if err != nil {
+		fmt.Println("Cannot use clipboard: ", err)
+	} else {
+		supportClipboard = true
+	}
+
+	// 0.2: load flags
 	flag.StringVar(&URL, "url", "", "URL to download")
 	flag.StringVar(&DIR, "dir", "", "Directory to save file")
+	flag.BoolVar(&useClipboard, "c", false, "Use clipboard mechanism")
 	flag.Parse()
 
+	// 0.3: validate given URL/DIR; fallback prompt.
 	if URL == "" { // fallback URL prompt
 		fmt.Print("Enter URL: ")
 		fmt.Scanln(&URL)
 	}
 
-	if DIR == "" { // fallback DIR prompt
+	if DIR == "" && !useClipboard { // fallback DIR prompt
 		fmt.Print("Enter DIR: ")
 		fmt.Scanln(&DIR)
 	}
 
-	if URL == "" || DIR == "" {
+	// 0.4: final validation of URL/DIR; display usage info if not successful.
+	if URL == "" || (DIR == "" && !useClipboard) {
 		flag.Usage()
 		os.Exit(1)
 	}
 
+	// 1: fetching data
+	// 1.1: create client and fetch HTML from the URL
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
 		log.Fatalf("unable to create request: %s", err)
 	}
 
+	// this is important because you can get banned.
 	req.Header.Set("User-Agent", WorkingUserAgent)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalf("unable to do request: %s", err)
 	}
 
+	// 1.2: read response
 	output := make([]byte, 0)
 	// read all content of response.Body
 	// into output
@@ -61,25 +85,45 @@ func main() {
 		}
 	}
 
+	// 1.3: setup document analysis
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(output)))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// 1.4: extract description text
 	// Find the div with the specified class
 	div := doc.Find("div.typography")
 
 	// Extract and print the content of the div
-	fmt.Println(strings.Split(div.Text(), "+48 660 887 000")[0]) // remove contact info
-	fmt.Println("----")
+	divStr := strings.Split(div.Text(), "+48 660 887 000")[0] // remove contact info
+	// 1.5: copy description to clipboard
+	if supportClipboard && useClipboard {
+		clipboard.Write(clipboard.FmtText, []byte(divStr))
+		fmt.Print("Description copied, press enter to get table...")
+		fmt.Scanln()
+	} else {
+		fmt.Println(divStr)
+	}
+
+	table := bytes.NewBufferString("-----")
 
 	// Extract table
 	div = doc.Find("div.productDetails__wrap").Find("div.productParams").Find("div.row")
 	names := div.Find("div.productParams__name")
 	params := div.Find("div.productParams__param")
 	for i, d := range params.Nodes {
-		fmt.Printf("%s : %s\n", names.Nodes[i].FirstChild.Data, d.FirstChild.Data)
+		fmt.Fprintf(table, "%s : %s\n", names.Nodes[i].FirstChild.Data, d.FirstChild.Data)
+	}
+
+	// 1.6: print table or paste to clipboard
+	if supportClipboard && useClipboard {
+		clipboard.Write(clipboard.FmtText, table.Bytes())
+		fmt.Print("Table copied, press enter to download images...")
+		fmt.Scanln()
+	} else {
+		fmt.Println(table.String())
 	}
 
 	// do image magic
@@ -88,6 +132,7 @@ func main() {
 	if !found {
 		log.Fatal("Cannot extract imaage")
 	}
+
 	imgURLs = append(imgURLs, mainURL)
 
 	imgURL := doc.Find("div.productFoto__sliderList").Find("img") //.Attr("src")
@@ -101,8 +146,10 @@ func main() {
 		}
 	}
 
-	if err = os.Mkdir(DIR, os.ModePerm); err != nil {
-		log.Fatalf("unable to create directory: %s", err)
+	if !useClipboard {
+		if err = os.Mkdir(DIR, os.ModePerm); err != nil {
+			log.Fatalf("unable to create directory: %s", err)
+		}
 	}
 
 	for i, url := range imgURLs {
@@ -117,16 +164,28 @@ func main() {
 			log.Fatalf("unable to do request: %s", err)
 		}
 
-		file, err := os.Create(filepath.Join(DIR, fmt.Sprintf("image%d.jpg", i)))
-		if err != nil {
-			log.Fatalf("unable to create file: %s", err)
-		}
+		if supportClipboard && useClipboard {
+			imgData := bytes.NewBufferString("")
+			_, err = io.Copy(imgData, resp.Body)
+			if err != nil {
+				log.Fatalf("unable to copy image data: %s", err)
+			}
 
-		defer file.Close()
+			clipboard.Write(clipboard.FmtImage, imgData.Bytes())
+			fmt.Printf("Image %d copied to clipboard, prese Enter for next...", i)
+			fmt.Scanln()
+		} else {
+			file, err := os.Create(filepath.Join(DIR, fmt.Sprintf("image%d.jpg", i)))
+			if err != nil {
+				log.Fatalf("unable to create file: %s", err)
+			}
 
-		_, err = io.Copy(file, resp.Body)
-		if err != nil {
-			log.Fatalf("unable to copy file: %s", err)
+			defer file.Close()
+
+			_, err = io.Copy(file, resp.Body)
+			if err != nil {
+				log.Fatalf("unable to copy file: %s", err)
+			}
 		}
 	}
 }
